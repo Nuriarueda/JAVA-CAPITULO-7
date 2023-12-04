7ef84952f97871f8db176ceebb687d3e547d4a91
Nuriarueda <ruedareinanuria@gmail.com>
2023-12-04 09:43:37 +0100

    capitulo 7

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,4 +1,4 @@
-public class Ejercicio8 {
+public class Ejercicio10 {
     public static void main(String[] args) {
         
     }
--- Ejercicio11.java
+++ Ejercicio11.java
@@ -1,4 +1,4 @@
-public class Ejercicio8 {
+public class Ejercicio11 {
     public static void main(String[] args) {
         
     }
