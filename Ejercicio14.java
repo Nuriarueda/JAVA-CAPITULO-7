e4ca18fc48a8a32d7cf4f07fe66eb14cb14a81cd
Nuriarueda <ruedareinanuria@gmail.com>
2023-12-06 10:43:02 +0100

    capitulo 7

--- Ejercicio14.java
+++ Ejercicio14.java
@@ -4,8 +4,36 @@ comienzo y las que no son colores a continuación. Puedes utilizar tantos arrays
 auxiliares como quieras. Los colores que conoce el programa deben estar en
 otro array y son los siguientes: verde, rojo, azul, amarillo, naranja, rosa, negro,
 blanco y morado. */
-public class Ejercicio14 {
-    public static void main(String[] args) {
+import java.util.Scanner;
+public class Ejercicio14{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa destaca el numero minimo o maximo");
+        String[] array= new String[8];
+
+        for(int i=0;i<8;i++){
+            array[i]=sc.next();
+        }
+
+        for(int i=0;i<8;i++){
+            if(array[i].equals("rosa") || array[i].equals("azul") || array[i].equals("verde") || array[i].equals("morado") || array[i].equals("rojo") || array[i].equals("negro") || array[i].equals("blanco") || array[i].equals("naranja") || array[i].equals("verde") || array[i].equals("amarillo")){
+                System.out.print(array[i]+" ");;
+            }
+        } 
         
-    }
-}
+        boolean imprimir;
+        
+        for(int i=0;i<8;i++){
+            imprimir=true;
+            if(array[i].equals("rosa") || array[i].equals("azul") || array[i].equals("verde") || array[i].equals("morado") || array[i].equals("rojo") || array[i].equals("negro") || array[i].equals("blanco") || array[i].equals("naranja") || array[i].equals("verde") || array[i].equals("amarillo")){
+                imprimir=false;
+            }
+
+            if(imprimir){
+                System.out.print(array[i]+" ");;
+            }
+        } 
+
+        sc.close();
+        }
+	}
--- Ejercicio16.java
+++ Ejercicio16.java
@@ -3,8 +3,54 @@ aleatorios comprendidos entre 0 y 400 (ambos incluidos). A continuación el
 programa mostrará el array y preguntará si el usuario quiere resaltar los
 múltiplos de 5 o los múltiplos de 7. Seguidamente se volverá a mostrar el array
 escribiendo los números que se quieren resaltar entre corchetes. */
+import java.util.Scanner;
 public class Ejercicio16 {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[20];
+
         
+        for(int i=0;i<20;i++){
+            array[i]=(int)(Math.random()*401);
+        }
+        
+        for(int i=0;i<20;i++){
+            System.out.print(array[i]+" ");
+        }
+
+        System.out.println();
+        System.out.print("¿Qué números quiere resaltar? (1.Los múltiplos de 5 , 2.Los múltiplos de 7): ");
+        int decision=sc.nextInt();
+
+        switch(decision){
+
+            case 1:
+            
+            for(int i=0;i<20;i++){
+                if(array[i]%5==0){
+                    System.out.print("["+array[i]+"] ");
+                }else{
+                    System.out.print(array[i]+" ");
+                }
+            }
+            
+            break;
+ 
+            case 2:
+            
+                for(int i=0;i<20;i++){
+                    if(array[i]%7==0){
+                        System.out.print("["+array[i]+"] ");
+                    }else{
+                        System.out.print(array[i]+" ");
+                    }
+                }
+
+            break;
+
+        }
+
+      sc.close();
     }
-}
+  }
