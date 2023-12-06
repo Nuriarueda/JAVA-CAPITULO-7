51ce6a899a4d4cfe32eb61a195598f10e182a747
Nuriarueda <ruedareinanuria@gmail.com>
2023-12-06 10:46:25 +0100

    capitulo 7

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -4,8 +4,46 @@ un array. A continuación se mostrará el contenido de ese array junto al índic
 primos a las primeras posiciones, desplazando el resto de números (los que no
 son primos) de tal forma que no se pierda ninguno. Al final se debe mostrar el
 array resultante. */
-public class Ejercicio11 {
-    public static void main(String[] args) {
-        
-    }
-}
+import java.util.Scanner;
+public class Ejercicio11{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa ordena el array por primos y no primos");
+        int[] array= new int[10];
+
+        for(int i=0;i<10;i++){
+            array[i]=sc.nextInt();
+        }
+
+        //declaramos una variable boolean para comprobar si son primos
+
+        boolean primo=true;
+        for(int i=0;i<10;i++){
+            primo=true;
+
+                for(int j=2;j<array[i];j++){
+                    if(array[i]%j==0){
+                        primo=false;
+                    }
+                }
+                if(primo){
+                    System.out.print(array[i]+" ");
+                }
+            }
+            
+            for(int i=0;i<10;i++){
+                primo=true;
+                
+                for(int j=2;j<array[i];j++){
+                    if(array[i]%j==0){
+                        primo=false;
+                    }
+                }
+                if(!primo){
+                    System.out.print(array[i]+" ");
+                }
+            }
+            
+        sc.close();
+        }
+	}
