2d6f89f562cda2eb6d9f8ce62433437921639444
Nuriarueda <ruedareinanuria@gmail.com>
2023-12-06 10:47:30 +0100

    capitulo 7

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,7 +1,21 @@
 /* Realiza un programa que pida 8 números enteros y que luego muestre esos
 números junto con la palabra “par” o “impar” según proceda. */
-public class Ejercicio9 {
-    public static void main(String[] args) {
-        
-    }
-}
+import java.util.Scanner;
+public class Ejercicio9{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa te dira si los numeros son pares o impares");
+        int [] array = new int[8];
+        for(int i=0;i<8;i++){
+            array[i]=sc.nextInt();
+        }
+        for(int i=0;i<8;i++){
+            if(array[i]%2==0){
+                System.out.println(array[i]+" es par");
+            }else{
+                System.out.println(array[i]+" es impar");
+            }
+        }
+        sc.close();
+        }
+	}
